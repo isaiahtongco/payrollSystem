9753f8082785370f743244015fd65a989d2274ed
+639988404975 <isaiahtongco@gmail.com>
2017-07-07 16:47:07 +0800

    TestComment

--- Class1.cs
+++ Class1.cs
@@ -11,6 +11,7 @@ public class Class1
         // TODO: Add constructor logic here
         //
         Console.WriteLine("Test");
+        Console.WriteLine("Isaiah")
         Console.ReadLine();
         //Update mo to pre dito sa site
 	}
